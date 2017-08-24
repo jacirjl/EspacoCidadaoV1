9e03639a6179182fbc790444cff841f00f24a6ee
jacirjl <jacirjl@hotmail.com>
2017-08-24 18:09:26 -0300

    Backup versão 1.0

--- app/src/main/java/com/example/jacirjlseae/espacocidadao/MainActivity.java
+++ app/src/main/java/com/example/jacirjlseae/espacocidadao/MainActivity.java
@@ -30,11 +30,6 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, SemECs.class));
             }
         });
-        botaoAtivos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
     }
 }
